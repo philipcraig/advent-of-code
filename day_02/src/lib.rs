9180c8ef9d3d540e3f77c2d5cc807b13aed2c04c
Philip Craig <689193+philipcraig@users.noreply.github.com>
2020-12-06 14:55:44 +0000

    day_02

--- day_02/src/lib.rs
+++ day_02/src/lib.rs
@@ -1,7 +1,72 @@
-pub fn part_01(input: &()) -> () {
-    todo!()
+#[macro_use]
+extern crate lazy_static;
+extern crate regex;
+
+use regex::Regex;
+
+macro_rules! err {
+    ($($tt:tt)*) => { Err(Box::<dyn Error>::from(format!($($tt)*))) }
 }
 
-pub fn part_02(input: &()) -> () {
-    todo!()
+use std::{error::Error, str::FromStr};
+
+pub fn part_01(input: &Vec<Line>) -> usize {
+    input.iter().filter(|l| is_valid_part1(*l)).count()
+}
+
+pub fn part_02(input: &Vec<Line>) -> usize {
+    input.iter().filter(|l| is_valid_part2(*l)).count()
+}
+
+fn is_valid_part1(l: &Line) -> bool {
+    let c = l.password.matches(l.valid).count();
+    
+    c >= l.lower && c <= l.upper
+}
+
+fn is_valid_part2(l: &Line) -> bool {
+    let first = l.valid == l.password.as_bytes()[l.lower - 1] as char;
+    let second = l.valid == l.password.as_bytes()[l.upper - 1] as char;
+
+    first ^ second
+}
+
+#[derive(Debug)]
+pub struct Line {
+    lower: usize,
+    upper: usize,
+    valid: char,
+    password: String,
+}
+
+impl FromStr for Line {
+    type Err = Box<dyn Error>;
+
+    fn from_str(s: &str) -> Result<Line, Box<dyn Error>> {
+        lazy_static! {
+            static ref RE: Regex = Regex::new(
+                r"(?x)
+                (?P<lower>[0-9]+)
+                -
+                (?P<upper>[0-9]+)
+                \s+
+                (?P<valid>[a-z])
+                :\s+
+                (?P<password>[a-z]+)
+            "
+            )
+            .unwrap();
+        }
+
+        let caps = match RE.captures(s) {
+            None => return err!("unrecognized line"),
+            Some(caps) => caps,
+        };
+        Ok(Line {
+            lower: caps["lower"].parse()?,
+            upper: caps["upper"].parse()?,
+            valid: caps["valid"].parse()?,
+            password: caps["password"].parse()?,
+        })
+    }
 }
--- day_02/src/main.rs
+++ day_02/src/main.rs
@@ -1,4 +1,6 @@
-use std::time::Instant;
+use std::{str::FromStr, time::Instant};
+
+use day_02::Line;
 
 fn main() {
     let input = parse_input(include_str!("../input"));
@@ -19,8 +21,13 @@ fn main() {
     println!("Part 2: {:?} ({:.2?})", part_02, now.elapsed());
 }
 
-fn parse_input(include_str: &str) -> () {
-    todo!()
+fn parse_input(input: &str) -> Vec<Line> {
+    input
+        .lines()
+        .map(|l| l.trim())
+        .filter(|l| !l.is_empty())
+        .map(|l| Line::from_str(l).unwrap())
+        .collect()
 }
 
 #[cfg(test)]
@@ -30,28 +37,22 @@ mod tests {
     #[test]
     fn test_data_part_01() {
         let data = "
-1721
-979
-366
-299
-675
-1456
+1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc
 ";
 
-        assert_eq!(day_02::part_01(&parse_input(data)), 514579)
+        assert_eq!(day_02::part_01(&parse_input(data)), 2)
     }
 
     #[test]
     fn test_data_part_02() {
         let data = "
-1721
-979
-366
-299
-675
-1456
+1-3 a: abcde
+1-3 b: cdefg
+2-9 c: ccccccccc
 ";
 
-        assert_eq!(day_02::part_02(&parse_input(data)), 241861950)
+        assert_eq!(day_02::part_02(&parse_input(data)), 1)
     }
 }
